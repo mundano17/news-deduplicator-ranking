9f90828a201bfd35cb4222edc7ddc785b26ca974
Purushotham <yallurupurushothamreddy006@gmail.com>
2026-03-02 22:39:10 +0530

    added dedup and ranking logic

--- clustering.py
+++ clustering.py
@@ -1,6 +1,3 @@
-# 1 FIX THE GODDAMN CLUSTERING
-# 2 RANK EM CLUSTERS
-# FIGURE OUT THE FORMAT FOR RANKING
 # 3 DEDUPLICATE DONE ?
 # 4 SCALE UP, MORE DATA.
 
@@ -17,7 +14,7 @@ from models import Scrape
 nlp = spacy.load("en_core_web_sm")
 
 
-class Clustering:
+class ClusteRanking:
     def __init__(self, text: list[Scrape]):
         self.text = text
         self.device = "cpu"
@@ -41,12 +38,12 @@ class Clustering:
         cluster = clusterer.fit(self.embeddings.cpu().numpy())
         clusterframe = pd.DataFrame(
             {
+                "url": [obj.url for obj in self.text],
+                "headlines": [obj.headline for obj in self.text],
                 "text": [obj.text for obj in self.text],
                 "cluster_id": cluster.labels_,
             }
         )
-        print(clusterframe)
-        print(f"DEBUG: Type of clusterframe is {type(clusterframe)}")
         return clusterframe
 
     def preprocessing(self, text: str):
@@ -71,8 +68,8 @@ class Clustering:
 
             return max(factors, 1)
 
-        x = get_factors(tokens)
-        y = get_factors(tokens[::-1])
+        x = len(tokens) / get_factors(tokens)
+        y = len(tokens) / get_factors(tokens[::-1])
         return (x + y) / 2
 
     def ranking(self, clusterframe: pd.DataFrame):
@@ -80,10 +77,37 @@ class Clustering:
         noiseless_frame["mtld_score"] = noiseless_frame["text"].apply(
             lambda x: self.mtld(self.preprocessing(x))
         )
-        print(noiseless_frame)
         return noiseless_frame
+
+    def ranking_hl(self, clusterframe: pd.DataFrame):
+        noiseless_frame = clusterframe[clusterframe["cluster_id"] != -1].copy()
+        noiseless_frame["mtld_score"] = noiseless_frame["headlines"].apply(
+            lambda x: self.mtld(self.preprocessing(x))
+        )
+        return noiseless_frame
+
+    def de_duplication(self, ranked_frame: pd.DataFrame):
+        ranked_frames = ranked_frame.sort_values(
+            by=["cluster_id", "mtld_score"], ascending=[True, False]
+        )
+        ranked_frames = ranked_frames.drop_duplicates(
+            subset=["cluster_id"], keep="first"
+        )
+        return ranked_frames
 
     def __call__(self):
         clusterframe = self.clustering()
+        lonely_stories = clusterframe[clusterframe["cluster_id"] == -1].copy()
         ranked_frame = self.ranking(clusterframe)
-        return ranked_frame
+        ranked_frames = self.de_duplication(ranked_frame)
+        final_frames = pd.concat([ranked_frames, lonely_stories], ignore_index=True)
+
+        print("### ORIGINAL CLUSTERING ###\n")
+        print(clusterframe)
+
+        print("### RANKED CLUSTERING ###")
+        print(ranked_frame)
+
+        print("### DEDUP + RANKED ###")
+        print(final_frames)
+        return final_frames
